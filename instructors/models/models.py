ba5609a3a268fa5aebc1caa67903b19c466d4f56
Mohamed Magdy <moh.magdy40@gmail.com>
2019-08-25 13:26:10 +0200

    [IMP] Improve readability by reformating the code.

--- instructors/models/models.py
+++ instructors/models/models.py
@@ -1,5 +1,5 @@
 from odoo import api, fields, models
-from odoo import exceptions
+
 
 class Instructor(models.Model):
     _name = 'ems.course.instructor'
@@ -9,37 +9,21 @@ class Instructor(models.Model):
         vals['sequence'] = self.env['ir.sequence'].next_by_code('ems.course.instructor')
         return super(Instructor, self).create(vals)
 
-
     @api.multi
-    @api.constrains('hour_price','working_hours','age')
+    @api.constrains('hour_price', 'working_hours', 'age')
     def check_hours(self):
-        if (self.working_hours and self.hour_price and self.age):
-            if ((self.working_hours and self.hour_price)< 0):
+        if self.working_hours and self.hour_price and self.age:
+            if (self.working_hours and self.hour_price) < 0:
                 raise ValueError("The number of hours is invalid")
-            if not (self.age >= 25 and self.age <= 60):
+            if not (25 <= self.age <= 60):
                 raise ValueError("The age is invalid")
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     sequence = fields.Char(string="ID", required=False, )
     name = fields.Char(string="Instructor Name :", required=True, )
     age = fields.Float(string="Age :", required=False, )
     address = fields.Char(string="Address", required=False, )
-    #TODO: allowed couses M2M with ems.course
-    hour_price = fields.Float(string="Hour price:",  required=False, )
-    working_hours = fields.Float(string="Working hours per month:",  required=False, )
-    allowed_branches_ids = fields.Many2many(comodel_name="ems.branch", relation="", column1="", column2="", string="Allowed Branches", required=True )
-
-
-
+    # TODO: allowed couses M2M with ems.course
+    hour_price = fields.Float(string="Hour price:", required=False, )
+    working_hours = fields.Float(string="Working hours per month:", required=False, )
+    allowed_branches_ids = fields.Many2many(comodel_name="ems.branch", relation="", column1="", column2="",
+                                            string="Allowed Branches", required=True)
