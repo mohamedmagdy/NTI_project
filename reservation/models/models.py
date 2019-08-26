3341ebb3f5043fd36a20bef6a4093db93c1a6f65
Khaled Hammad <khaled.hammad5050@gmail.com>
2019-08-26 16:16:07 +0200

    [FIX]  fix the names of the relational fields

--- reservation/models/models.py
+++ reservation/models/models.py
@@ -9,25 +9,24 @@ class Reservation(models.Model):
     _description = 'Course Reservation'
     _inherit = 'mail.thread'
 
-    # FIXME: Not-urgent. Please, fix the names of the relational fields
     # Initialize Database Fields
     sequence = fields.Char(string="ID", required=False, )
     select_course = fields.Many2one(comodel_name="ems.course", string="Select Course/Package", required=True,
                                     track_visibility="onchange")
-    select_round = fields.Many2one(comodel_name="ems.course.round", string="Select Round ", required=True,
+    select_round_id = fields.Many2one(comodel_name="ems.course.round", string="Select Round ", required=True,
                                    track_visibility="onchange")
-    select_round_status = fields.Many2one(comodel_name="ems.round.status", string="Select Round Status",
+    select_round_status_id = fields.Many2one(comodel_name="ems.round.status", string="Select Round Status",
                                           required=True, track_visibility="onchange")
-    select_round_type = fields.Many2one(comodel_name="ems.round.types", string="Select Round Type", required=True,
+    select_round_type_id = fields.Many2one(comodel_name="ems.round.types", string="Select Round Type", required=True,
                                         track_visibility="onchange")
 
     # Initialize Related Fields With ems.course.round Table
-    round_days = fields.Selection(string="Round Days", related="select_round.round_days")
-    session_count = fields.Integer(string="Session count", related="select_round.sessions_count")
-    start_date = fields.Date(string="Start Date", related="select_round.start_date")
-    end_date = fields.Date(string="End Date", related="select_round.end_date")
-    location = fields.Many2one(string="Location", related="select_round.location")
-    round_time = fields.Char(string="Round Time", related="select_round.round_time")
+    round_days = fields.Selection(string="Round Days", related="select_round_id.round_days")
+    session_count = fields.Integer(string="Session count", related="select_round_id.sessions_count")
+    start_date = fields.Date(string="Start Date", related="select_round_id.start_date")
+    end_date = fields.Date(string="End Date", related="select_round_id.end_date")
+    location_id = fields.Many2one(string="Location", related="select_round_id.location")
+    round_time = fields.Char(string="Round Time", related="select_round_id.round_time")
 
     # Create Sequence Function
     @api.model
