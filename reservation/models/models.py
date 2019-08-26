62587ef5cbea89d18212bc03d8f89c67b168dab8
Amr Aly <amraly_267@yahoo.com>
2019-08-26 17:23:03 +0200

    [ADD] Add track visibility to the fields to track changes.

--- reservation/models/models.py
+++ reservation/models/models.py
@@ -16,13 +16,20 @@ class Reservation(models.Model):
     select_round = fields.Many2one(comodel_name="ems.course.round", string="Select Round ", required=True,
                                    track_visibility="onchange")
     select_round_status = fields.Many2one(comodel_name="ems.round.status", string="Select Round Status",
-                                          required=True,  track_visibility="onchange")
+                                          required=True, track_visibility="onchange")
     select_round_type = fields.Many2one(comodel_name="ems.round.types", string="Select Round Type", required=True,
                                         track_visibility="onchange")
-    round_days = fields.Selection(string="Round Days", related="select_round.round_days", required=True,
-                                  track_visibility="onchange")
+
+    # Initialize Related Fields With ems.course.round Table
+    round_days = fields.Selection(string="Round Days", related="select_round.round_days")
     session_count = fields.Integer(string="Session count", related="select_round.sessions_count")
     start_date = fields.Date(string="Start Date", related="select_round.start_date")
     end_date = fields.Date(string="End Date", related="select_round.end_date")
     location = fields.Many2one(string="Location", related="select_round.location")
     round_time = fields.Char(string="Round Time", related="select_round.round_time")
+
+    # Create Sequence Function
+    @api.model
+    def create(self, vals):
+        vals['sequence'] = self.env['ir.sequence'].next_by_code('reservation.reservation')
+        return super(Reservation, self).create(vals)
