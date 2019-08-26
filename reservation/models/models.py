9b04643f76fac6e4f2415ae33c33150858a6726d
Amr Aly <amraly_267@yahoo.com>
2019-08-26 13:08:09 +0200

    [ADD] Add all required related fields round days, session count, start date, end date, location and round time that depend on course round in both model and view

--- reservation/models/models.py
+++ reservation/models/models.py
@@ -3,21 +3,24 @@
 from odoo import models, fields, api
 
 
-class reservation(models.Model):
+class Reservation(models.Model):
     _name = 'reservation.reservation'
     _rec_name = 'reservation_code'
     _description = 'New Description'
 
-    reservation_code = fields.Char(string="ID", required=True, )
-    select_course = fields.Many2one(comodel_name="ems.course", string="Select Course/Package Name", required=True, )
-    select_round = fields.Many2one(comodel_name="ems.course.rounds", string="Select Round ", required=True, )
+    reservation_code = fields.Char(string="ID", required=True,  track_visibility="onchange")
+    select_course = fields.Many2one(comodel_name="ems.course", string="Select Course/Package Name", required=True,
+                                    track_visibility="onchange")
+    select_round = fields.Many2one(comodel_name="ems.course.round", string="Select Round ", required=True,
+                                   track_visibility="onchange")
     select_round_status = fields.Many2one(comodel_name="ems.round.status", string="Select Round Status",
-                                          required=True, )
-    select_round_type = fields.Many2one(comodel_name="ems.round.types", string="Select Round Type", required=True, )
-    log = fields.Html(string="Log", )
-    # TODO:Round Days  :  related field = round.days
-    # TODO:Sessions Count  :  related field = round.sessions_count
-    # TODO:Start Date :  related field = round.start_date
-    # TODO:End Date :  :  related field = round.end_date
-    # TODO:Location :  related field = round.location
-    # TODO:Round Time : related field = round.location
+                                          required=True,  track_visibility="onchange")
+    select_round_type = fields.Many2one(comodel_name="ems.round.types", string="Select Round Type", required=True,
+                                        track_visibility="onchange")
+    round_days = fields.Selection(string="Round Days", related="select_round.round_days", required=True,
+                                  track_visibility="onchange")
+    session_count = fields.Integer(string="Session count", related="select_round.sessions_count")
+    start_date = fields.Date(string="Start Date", related="select_round.start_date")
+    end_date = fields.Date(string="End Date", related="select_round.end_date")
+    location = fields.Many2one(string="Location", related="select_round.location")
+    round_time = fields.Char(string="Round Time", related="select_round.round_time")
