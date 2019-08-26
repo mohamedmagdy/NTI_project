7687e89773754fc49cf87eda0153a2ba42558723
MohammedAlii80 <ma643215@gmail.com>
2019-08-26 09:33:50 +0200

    [ADD] add instructor allocation model

--- instructors/models/models.py
+++ instructors/models/models.py
@@ -23,8 +23,26 @@ class Instructor(models.Model):
     age = fields.Float(string="Age", required=False, )
     address = fields.Char(string="Address", required=False, )
     # TODO: allowed couses M2M with ems.course
+    allowed_couses_ids = fields.Many2many(comodel_name="ems.course", relation="instructor_course_rel",
+                                     column1="instructor_id", column2="course_id",
+                                     string="Allowed couses", )
     hour_price = fields.Float(string="Hour price", required=False, )
     working_hours = fields.Float(string="Working hours per month", required=False, )
     allowed_branches_ids = fields.Many2many(comodel_name="ems.branch", relation="instructor_branch_rel",
                                             column1="instructor_id", column2="branch_id", string="Allowed Branches",
                                             required=True)
+
+class InstructorAllocation(models.Model):
+    _name = 'ems.courses.instructors.allocation'
+    _description = 'New Description'
+
+    sequence = fields.Char(string="ID", required=False, )
+    instructor = fields.Many2one(comodel_name="ems.course.instructor", string="Instructors allocation"
+                                 , required=False, )
+
+    round = fields.Many2one(comodel_name="ems.course.round", string="Round", required=False, )
+    date_time_from = fields.Datetime(string="Date Time From", required=False, )
+    date_time_to = fields.Datetime(string="Date Time To", required=False, )
+
+    # TODO: constraints
+        
