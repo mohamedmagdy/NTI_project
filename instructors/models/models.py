851057d998cb0ba3a744879f9498c23045b579a8
MohammedAlii80 <ma643215@gmail.com>
2019-08-26 14:41:15 +0200

    [ADD] add constrains and new view form and tree

--- instructors/models/models.py
+++ instructors/models/models.py
@@ -1,4 +1,5 @@
 from odoo import api, fields, models
+from datetime import  datetime
 
 
 class Instructor(models.Model):
@@ -22,27 +23,51 @@ class Instructor(models.Model):
     name = fields.Char(string="Instructor Name", required=True, )
     age = fields.Float(string="Age", required=False, )
     address = fields.Char(string="Address", required=False, )
-    # TODO: allowed couses M2M with ems.course
-    allowed_couses_ids = fields.Many2many(comodel_name="ems.course", relation="instructor_course_rel",
-                                     column1="instructor_id", column2="course_id",
-                                     string="Allowed couses", )
+    allowed_courses_ids = fields.Many2many(comodel_name="ems.course", relation="instructor_course_rel",
+                                           column1="instructor_id", column2="course_id", string="Allowed courses", )
     hour_price = fields.Float(string="Hour price", required=False, )
     working_hours = fields.Float(string="Working hours per month", required=False, )
     allowed_branches_ids = fields.Many2many(comodel_name="ems.branch", relation="instructor_branch_rel",
                                             column1="instructor_id", column2="branch_id", string="Allowed Branches",
                                             required=True)
 
+
 class InstructorAllocation(models.Model):
     _name = 'ems.courses.instructors.allocation'
     _description = 'New Description'
 
-    sequence = fields.Char(string="ID", required=False, )
-    instructor = fields.Many2one(comodel_name="ems.course.instructor", string="Instructors allocation"
-                                 , required=False, )
+    @api.model
+    def create(self, vals):
+        vals['sequence'] = self.env['ir.sequence'].next_by_code('ems.courses.instructors.allocation')
+        return super(InstructorAllocation, self).create(vals)
 
-    round = fields.Many2one(comodel_name="ems.course.round", string="Round", required=False, )
+
+    @api.multi
+    @api.constrains('date_time_from','date_time_to')
+    def check_time(self):
+        for record in self:
+            same_day= record.date_time_from.date()
+            next_day= record.date_time_to.date()
+            if not  (same_day == next_day):
+                raise ValueError('Not the same Day!')
+
+    # @api.multi
+    # @api.constrains('date_time_from','date_time_to')
+    # def check_date_validation(self):
+    #     if not (self.date_time_from.date() >= self.round_ids.start_date.date() and self.date_time_to.date() <= self.round_ids.end_date.date()):
+    #         raise ValueError("Time must be between start day and End Day")
+
+
+
+
+
+    sequence = fields.Char(string="ID", required=False, )
+    instructor = fields.Many2one(comodel_name="ems.course.instructor", string="Instructors allocation",
+                                 required=False, )
+
+
+    round_ids = fields.Many2many(comodel_name="ems.course.round", relation="round_instructor_rel", column1="instructor_id", column2="round_id", string="Course Round", )
     date_time_from = fields.Datetime(string="Date Time From", required=False, )
     date_time_to = fields.Datetime(string="Date Time To", required=False, )
 
     # TODO: constraints
-        
