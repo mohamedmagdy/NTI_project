a691864da2207037042d943be531528ddc588a37
MSalahWeb <m.salah.ahmed@hotmail.com>
2019-09-03 14:50:37 +0200

    [ADD] Add new features.

--- course_round/models/models.py
+++ course_round/models/models.py
@@ -9,13 +9,9 @@ class Round(models.Model):
 
     sequence = fields.Char(string="ID", required=False, default='New', readonly=True)
     course_id = fields.Many2one(comodel_name="ems.course", string="Course ID", required=True, )
-    location_id = fields.Many2one(comodel_name="ems.branch", string="Branch Location", required=True, )
-    round_status_ids = fields.Many2many(comodel_name="ems.round.status", relation="round_round_status_rel",
-                                        column1="round_id", column2="round_status_id", string="Round Status",
-                                        required=True)
-    round_type_ids = fields.Many2many(comodel_name="ems.round.types", relation="round_round_types_rel",
-                                      column1="round_id",
-                                      column2="round_status_id", string="Round Types", required=True)
+    branch_id = fields.Many2one(comodel_name="ems.branch", string="Branch Location", required=True, )
+    lab_id = fields.Many2one(comodel_name="ems.branch.labs", string="Lab", required=False, )
+    round_types_id = fields.Many2one(comodel_name="ems.round.types", string="Round Type", required=True, )
     reservation_type_ids = fields.Many2many(comodel_name="ems.reservation.types",
                                             relation="round__reservation_type_rel",
                                             column1="round_id", column2="reservation_type_id", string="Reservations",
@@ -24,11 +20,14 @@ class Round(models.Model):
                                                                    ('sat-tue', 'Saturday-Tuesday'),
                                                                    ('sun-wed', 'Sunday-Wednesday'),
                                                                    ('mon-thu', 'Monday-Thursday')], required=True, )
-    sessions_count = fields.Integer(string="Sessions Count", required=True, )
+    course_hours = fields.Integer(string="Course Hours", required=True, )
+    session_hours = fields.Integer(string="Session Hours", required=False, default="1")
     start_date = fields.Date(string="Start Date", required=True, default=fields.Date.context_today)
     end_date = fields.Date(string="End Date", required=True, )
-    round_time = fields.Char(string="Round Time", required=False, )
-    # instructor_ids = fields.Many2many(comodel_name="ems.courses.instructors.allocation", relation="round_instructor_rel",
+    from_time = fields.Datetime(string="Time From", required=True, default=fields.Datetime.now)
+    to_time = fields.Datetime(string="Until", required=True, )
+    # instructor_ids = fields.Many2many(comodel_name="ems.courses.instructors.allocation",
+    # relation="round_instructor_rel",
     # column1="instructor_id", column2="round_id", string="Select Instructor", )
     state = fields.Selection(string="Status",
                              selection=[('draft', 'Draft'), ('confirm', 'Confirmed'), ('start', 'Started'),
@@ -36,9 +35,16 @@ class Round(models.Model):
     trainee_id = fields.Many2one(comodel_name="res.partner", string="Trainee", required=False, )
     ref = fields.Reference(string="Reference", selection=[('ems.course', 'Course'),
                                                           ('res.partner', 'Package')])
-    session_round_ids = fields.One2many(comodel_name="ems.course.session", inverse_name="round_session_id", string="Session", required=False, )
+    session_round_ids = fields.One2many(comodel_name="ems.course.session", inverse_name="round_id",
+                                        string="Session", required=False, )
+    sessions_count = fields.Integer(string="Session Count", required=False, )
 
     # TODO: log interface
+
+    @api.onchange('session_hours', 'course_hours')
+    def _onchange_session_count(self):
+        self.sessions_count = self.course_hours / self.session_hours
+        print(self.sessions_count)
 
     @api.onchange('sessions_count', 'start_date', 'round_days')
     def _onchange_end_date(self):
@@ -49,6 +55,10 @@ class Round(models.Model):
             countmethod = (self.sessions_count - 1) * 7
             self.end_date = self.start_date + datetime.timedelta(days=countmethod)
 
+    @api.onchange('session_hours', 'from_time')
+    def _onchange_to_time(self):
+        self.to_time = self.from_time + datetime.timedelta(hours=self.session_hours)
+
     _sql_constraints = [
         ('check_count', 'check(sessions_count > 0)', 'sessions count should be MORE THAN ZERO')
     ]
@@ -56,21 +66,25 @@ class Round(models.Model):
     @api.model
     def create(self, vals):
         vals['sequence'] = self.env['ir.sequence'].next_by_code('ems.course.round')
+        vals['session_round_ids'] = []
+        for i in range(vals['sessions_count']):
+            vals['session_round_ids'].append((0, 0, {'session_date': vals['start_date'], 'hours': vals['session_hours'], 'sequence': 'Sect-'+ str(i+1)}))
+
         return super(Round, self).create(vals)
 
 
 class Session(models.Model):
     _name = 'ems.course.session'
-    _rec_name = 'sequence_s'
+    _rec_name = 'round_id'
     _description = 'Describe Course Session'
 
-    sequence_s = fields.Char(string="Session", required=False, )
-    round_session_id = fields.Many2one(comodel_name="ems.course.round", string="Session", required=False, )
-    session_date = fields.Date(string="Session Date", required=True, )
-    instructor = fields.Selection(string="Instructor", selection=[('salah', 'Mohamed Salah'), ('essam', 'Mohamed Essam'), ], required=True, )
-    hours = fields.Selection(string="Hours", selection=[('1', '1 Hour'), ('2', '2 Hour'), ('3', '3 Hour'), ('4', '4 Hour'), ('5', '5 Hour'), ('6', '6 Hour'), ], required=False, )
+    round_id = fields.Many2one(comodel_name="ems.course.round", string="Session", required=False, )
+    sequence = fields.Char(string="ID", required=False, default='New', readonly=True)
+    session_date = fields.Date(string="Session Date", required=False, comodel_name="ems.course.round", rel="start_date")
+    instructor = fields.Selection(string="Instructor",
+                                  selection=[('salah', 'Mohamed Salah'), ('essam', 'Mohamed Essam'), ],
+                                  required=False, )
+    hours = fields.Integer(string="Hours", required=False, )
 
-    @api.model
-    def create(self, vals):
-        vals['sequence_s'] = self.env['ir.sequence'].next_by_code('ems.course.session')
-        return super(Session, self).create(vals)
+
+
