9a69132b778d30bad198bc26c8fbbebb7535f7f5
Amr Aly <amraly_267@yahoo.com>
2019-09-03 18:22:51 +0200

    [ADD] Add new fields after the update of analysis requirments

--- reservation/models/models.py
+++ reservation/models/models.py
@@ -14,19 +14,30 @@ class Reservation(models.Model):
     select_course = fields.Many2one(comodel_name="ems.course", string="Select Course/Package", required=True,
                                     track_visibility="onchange")
     select_round_id = fields.Many2one(comodel_name="ems.course.round", string="Select Round ", required=True,
-                                   track_visibility="onchange")
+                                      track_visibility="onchange")
     select_round_status_id = fields.Many2one(comodel_name="ems.round.status", string="Select Round Status",
-                                          required=True, track_visibility="onchange")
+                                             required=True, track_visibility="onchange")
     select_round_type_id = fields.Many2one(comodel_name="ems.round.types", string="Select Round Type", required=True,
-                                        track_visibility="onchange")
+                                           track_visibility="onchange")
+    source_of_knowing = fields.Char(string="Source Of Knowing", track_visibility="onchange", )
+    payment_type = fields.Selection(string="Payment Type",
+                                    selection=[('bank', 'Bank Transfer'), ('check', 'Check'), ('cash', 'Cash'), ],
+                                    required=True, track_visibility="onchange", )
+    student_id = fields.Many2one(comodel_name="res.partner", string="Student", required=True,
+                                 track_visibility="onchange", )
+
+    reservation_type = fields.Selection(string="Reservation Type",
+                                        selection=[('schedule', 'Schedule'), ('waiting', 'Waiting List'),
+                                                   ('online', 'Online'), ], required=False, )
+    branch_lab_id = fields.Many2one(comodel_name="ems.branch.labs", string="Branch Labs", required=False, )
+    seats = fields.Integer(string="Seats", required=False, relation='branch_lab_id.seats_count')
 
     # Initialize Related Fields With ems.course.round Table
     round_days = fields.Selection(string="Round Days", related="select_round_id.round_days")
     session_count = fields.Integer(string="Session count", related="select_round_id.sessions_count")
     start_date = fields.Date(string="Start Date", related="select_round_id.start_date")
     end_date = fields.Date(string="End Date", related="select_round_id.end_date")
-    location_id = fields.Many2one(string="Location", related="select_round_id.location")
-    round_time = fields.Char(string="Round Time", related="select_round_id.round_time")
+    round_time = fields.Datetime(string="Round Time", related="select_round_id.from_time")
 
     # Create Sequence Function
     @api.model
