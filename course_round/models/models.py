1b551426381753697761ceea27902e883cb73fe4
Mohamed Essam <mohamedmady61@gmail.com>
2019-09-08 14:40:20 +0200

    [IMP]Improve Methods

--- course_round/models/models.py
+++ course_round/models/models.py
@@ -47,37 +47,61 @@ class Round(models.Model):
 
     @api.onchange('sessions_count', 'start_date', 'round_days')
     def _onchange_end_date(self):
-        if self.round_days in ['sat-tue', 'sun-wed', 'mon-thu']:
-            countmethod = (self.sessions_count - 1) * 3.5
-            self.end_date = self.start_date + datetime.timedelta(days=countmethod)
+        if self.round_days in ['Saturday-tue', 'Sunday', 'Monday']:
+            count_method = (self.sessions_count - 1) * 3.5
+            self.end_date = self.start_date + datetime.timedelta(days=count_method)
         else:
-            countmethod = (self.sessions_count - 1) * 7
-            self.end_date = self.start_date + datetime.timedelta(days=countmethod)
+            count_method = (self.sessions_count - 1) * 7
+            self.end_date = self.start_date + datetime.timedelta(days=count_method)
 
     @api.onchange('session_hours', 'from_time')
     def _onchange_to_time(self):
-        self.to_time = self.from_time + datetime.timedelta(hours=self.session_hours)
+        self.to_time = self.from_time + self.session_hours
+
+    @api.onchange('start_date', 'week_day')
+    def _onchange_start_date(self):
+        self.week_day = self.start_date.strftime('%A')
 
     _sql_constraints = [
-        ('check_count', 'check(sessions_count > 0)', 'sessions count should be MORE THAN ZERO')
+        ('check_count', 'check(sessions_count > 0)', 'sessions count should be MORE THAN ZERO'),
+        ('week_day', 'check(week_day = round_days)', 'Start Date should be = Choose Days')
     ]
 
     @api.model
     def create(self, vals):
         vals['sequence'] = self.env['ir.sequence'].next_by_code('ems.course.round')
         vals['session_round_ids'] = []
-        for i in range(vals['sessions_count']):
-            calc_date = datetime.datetime.strptime(vals['start_date'], '%Y-%m-%d')
-            if vals['round_days'] in ['sat-tue', 'sun-wed', 'mon-thu']:
-                countmethod = (i * 3.5)
-                calc_dates = calc_date + datetime.timedelta(days=countmethod)
+        i = vals['sessions_count']
+        x = 1
+        _calc_date = datetime.datetime.strptime(vals['start_date'], '%Y-%m-%d')
+        calc_date = _calc_date.date()
+        calc_dates = calc_date
+        session_number = 1
+        days_off = self.env['ems.days.off'].search_read(domain=[], fields=['start_date', 'end_date'])
+        while x < i:
+            o = 0
+            if vals['round_days'] in ['Saturday-tue', 'Sunday', 'Monday']:
+                count_method = (x * 3.5)
             else:
-                countmethod = (i * 7)
-                calc_dates = calc_date + datetime.timedelta(days=countmethod)
-            vals['session_round_ids'].append((0, 0, {'session_date': calc_dates, 'hours': vals['session_hours'],
-                                                     'sequence': 'Sect-' + str(i + 1),
-                                                     'instructor': vals['trainee_id']}))
-
+                count_method = (x * 7)
+            for z in days_off:
+                if z['start_date'] <= calc_dates <= z['end_date']:
+                    calc_dates = calc_date + datetime.timedelta(days=count_method)
+                    i = i + 1
+                else:
+                    o = o + 1
+                    if o == len(days_off):
+                        vals['session_round_ids'].append(
+                            (0, 0, {'session_date': calc_dates, 'hours': vals['session_hours'],
+                                    'sequence': 'Sect-' + str(session_number),
+                                    'instructor': vals['instructor_id'],
+                                    'from_time_se': vals['from_time'],
+                                    'to_time_se': vals['to_time']}))
+                        vals['end_date'] = calc_dates
+                        calc_dates = calc_date + datetime.timedelta(days=count_method)
+                        session_number = session_number + 1
+                        print('o ', o, 'days_off ', len(days_off), 'x ', x, 'i ', i)
+            x = x + 1
         return super(Round, self).create(vals)
 
 
